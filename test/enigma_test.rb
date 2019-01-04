98aedb8fe6a015ca79607c0c30971411892fb669
Julia Marco <marcootj@gmail.com>
2019-01-04 11:48:15 -0700

    Added it has a character test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,11 @@ class EnigmaTest < MiniTest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_has_a_character_test
+
+    enigma = Enigma.new
+    character_set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal character_set, enigma.character_set
+  end
+
 end
