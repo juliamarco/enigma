db8571c72eaffeb0d4e53212bb325b0af81ddb37
Julia Marco <marcootj@gmail.com>
2019-01-04 11:52:23 -0700

    Added setup method to access enigma easier

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,17 +6,20 @@ require './lib/enigma'
 
 class EnigmaTest < MiniTest::Test
 
+  def setup
+    @enigma = Enigma.new
+  end
+
   def test_it_exists
 
-    enigma = Enigma.new
-    assert_instance_of Enigma, enigma
+    assert_instance_of Enigma, @enigma
   end
 
   def test_it_has_a_character_test
 
-    enigma = Enigma.new
     character_set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    assert_equal character_set, enigma.character_set
+    assert_equal character_set, @enigma.character_set
   end
+
 
 end
