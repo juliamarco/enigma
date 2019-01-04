7265cf54bf521f76f8727d4e668b383c79fe0b6e
Julia Marco <marcootj@gmail.com>
2019-01-04 09:56:33 -0700

    Added test_it_exist and Enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,15 @@
 require 'simplecov'
 SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < MiniTest::Test
+
+  def test_it_exists
+
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+end
