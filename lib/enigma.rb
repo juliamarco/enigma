e9bf3878018a8895677872f0378424758d061b7f
Julia Marco <marcootj@gmail.com>
2019-01-05 15:43:01 -0700

    Adds helper method to create a hash with all the indexes separated by their rotation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -26,10 +26,32 @@ class Enigma
     end
   end
 
-  def generate_shifts
+  def generate_shifts(keys, offsets)
+    generate_keys(keys)
+    generate_offsets(offsets)
     @shifts = @keys.merge(@offsets) do |letter, keys_num, offsets_num|
       keys_num.to_i + offsets_num.to_i
     end
+  end
+
+  def creates_hash_with_index_for_each_shift(string)
+    index_hash = { A: [], B: [], C: [], D: [] }
+    first_index = 0
+    all_indexes = (first_index..string.length).to_a
+    index_hash.each do |k,v|
+      all_indexes.each do |num|
+        index_hash[k] << num
+        3.times {all_indexes.shift}
+      end
+      first_index += 1
+      all_indexes = (first_index..string.length).to_a
+    end
+    return index_hash
+  end
+
+  def encrypt(string, keys, offsets)
+    generate_shifts(keys, offsets)
+    index_hash = creates_hash_with_index_for_each_shift(string)
   end
 
 
