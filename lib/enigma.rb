d1294e840270ff13736429e4d40a43fca71d4a0b
Julia Marco <marcootj@gmail.com>
2019-01-05 07:50:31 -0700

    Added generate keys method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,7 +4,17 @@ class Enigma
 
   def initialize
     @character_set = ("a".."z").to_a << " "
-    @keys = []
+    @keys = {A: 0, B: 0, C: 0, D: 0}
   end
+
+  def generate_keys(number)
+    number = number.to_s.split("")
+    number = ["0", "2", "7", "1", "5"]
+    @keys.each do |letter, num|
+      @keys[letter] = number.slice(0..1).join
+      number.shift
+    end
+  end
+
 
 end
