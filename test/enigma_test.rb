ffa794dca5bd213df8a589ed1548efe461459c72
Julia Marco <marcootj@gmail.com>
2019-01-05 12:43:21 -0700

    added starts with no shift test method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -37,7 +37,13 @@ class EnigmaTest < MiniTest::Test
     assert_equal expected, @enigma.generate_offsets(date)
   end
 
-  # def test_it
+  def test_it_starts_with_no_shifts
+
+    expected = {A: 0, B: 0, C: 0, D: 0}
+    assert_equal expected, @enigma.shifts
+  end
+
+
 
 
 end
