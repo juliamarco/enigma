b10b498cd5aacb0060a7c2447c18a3867b2e893f
Julia Marco <marcootj@gmail.com>
2019-01-06 14:28:20 -0700

    enigma is inheriting from the shiftgenerator class now

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,53 +1,14 @@
-class Enigma
+require 'shift_generator'
+
+class Enigma < ShiftGenerator
   attr_reader :character_set,
               :shifts
 
   def initialize
     @character_set = ("a".."z").to_a << " "
-    @keys = {A: 0, B: 0, C: 0, D: 0}
-    @offsets = {A: 0, B: 0, C: 0, D: 0}
     @shifts = {}
   end
 
-  def generate_keys(number)
-    number = number.to_s.split("")
-    @keys.each do |letter, num|
-      @keys[letter] = number.slice(0..1).join
-      number.shift
-    end
-  end
-
-  def generate_offsets(date)
-    date = date.to_i
-    number = (date * date).to_s[-4..-1].split("")
-    @offsets.each do |letter, num|
-      @offsets[letter] = number[0]
-      number.shift
-    end
-  end
-
-  def generate_shifts(key, date)
-    generate_keys(key)
-    generate_offsets(date)
-    @shifts = @keys.merge(@offsets) do |letter, keys_num, offsets_num|
-      keys_num.to_i + offsets_num.to_i
-    end
-  end
-
-  def creates_hash_with_index_for_each_shift(string)
-    index_hash = { A: [], B: [], C: [], D: [] }
-    first_index = 0
-    all_indexes = (first_index..string.length).to_a
-    index_hash.each do |k,v|
-      all_indexes.each do |num|
-        index_hash[k] << num
-        3.times {all_indexes.shift}
-      end
-      first_index += 1
-      all_indexes = (first_index..string.length).to_a
-    end
-    return index_hash
-  end
 
   def todays_date
     date = Date.today
