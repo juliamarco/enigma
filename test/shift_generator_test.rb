23479d1cd5ea7b9e131916faed4c5071176f50e3
Julia Marco <marcootj@gmail.com>
2019-01-06 14:04:21 -0700

    Added test_it_starts_with_no_shifts

--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -10,4 +10,11 @@ class ShiftGeneratorTest < MiniTest::Test
     assert_instance_of ShiftGenerator, shift_generator
   end
 
+  def test_it_starts_with_no_shifts
+
+    shift_generator = ShiftGenerator.new
+    expected = {}
+    assert_equal expected, shift_generator.shifts
+  end
+
 end
