5dc89616c686635756ecf8cdf59c85f04c48abe8
Julia Marco <marcootj@gmail.com>
2019-01-06 11:47:51 -0700

    Added keys_generator_test file

---
+++ test/keys_generator_test.rb
@@ -0,0 +1,15 @@
+require './lib/keys_generator'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class KeysGeneratorTest
+
+  KeysGeneratorTest < MiniTest::Test
+
+  def test_it_exists
+    keys = KeysGenerator.new
+
+    assert_instance_of KeysGenerator, keys
+  end
+
+end
