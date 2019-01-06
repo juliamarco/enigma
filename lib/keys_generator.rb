f2ab4144673ccd8d26a59249a8bbc30de9b20c05
Julia Marco <marcootj@gmail.com>
2019-01-06 13:50:19 -0700

    Added generate_keys method

--- lib/keys_generator.rb
+++ lib/keys_generator.rb
@@ -5,4 +5,12 @@ class KeysGenerator
     @keys = {A: 0, B: 0, C: 0, D: 0}
   end
 
+  def generate_keys(number)
+    number = number.to_s.split("")
+    @keys.each do |letter, num|
+      @keys[letter] = number.slice(0..1).join
+      number.shift
+    end
+  end
+
 end
