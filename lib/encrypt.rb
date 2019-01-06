7e8de3932e8312a698c34ba0009931b8f0f56acd
Julia Marco <marcootj@gmail.com>
2019-01-06 08:28:22 -0700

    writes encrypted message in encrypted.txt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,6 +3,15 @@ require 'date'
 
 handle = File.open(ARGV[0], "r")
 message = handle.read.chop
+handle.close
+
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(message)
-# require 'pry'; binding.pry
+encrypted = enigma.encrypt(message)
+
+file_name = ARGV[1]
+writer = File.open(file_name, "w")
+writer.write(encrypted)
+
+p "File: #{file_name}, key: #{encrypted[:key]}, date: #{encrypted[:date]}"
+
+writer.close
