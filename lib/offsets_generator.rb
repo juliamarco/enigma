cc73c80b8772d417e284a18987d6e483643cbbc0
Julia Marco <marcootj@gmail.com>
2019-01-06 13:58:32 -0700

    Added generate_offsets method for OffsetsGenerator

--- lib/offsets_generator.rb
+++ lib/offsets_generator.rb
@@ -5,4 +5,13 @@ class OffsetsGenerator
     @offsets = {A: 0, B: 0, C: 0, D: 0}
   end
 
+  def generate_offsets(date)
+    date = date.to_i
+    number = (date * date).to_s[-4..-1].split("")
+    @offsets.each do |letter, num|
+      @offsets[letter] = number[0]
+      number.shift
+    end
+  end
+
 end
