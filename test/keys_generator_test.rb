d79f2d3e7b165007a8b09c86ccbc06defe05eb80
Julia Marco <marcootj@gmail.com>
2019-01-06 14:19:32 -0700

    Changed test so they pass an argument when they initialize an Offset object

--- test/keys_generator_test.rb
+++ test/keys_generator_test.rb
@@ -5,23 +5,16 @@ require 'minitest/pride'
 class KeysGeneratorTest < MiniTest::Test
 
   def test_it_exists
-    keys = KeysGenerator.new
+    keys = KeysGenerator.new("02715")
 
     assert_instance_of KeysGenerator, keys
   end
 
-  def test_it_starts_with_no_keys
-
-    keys = KeysGenerator.new
-    expected = {A: 0, B: 0, C: 0, D: 0}
-    assert_equal expected, keys.keys
-  end
-
   def test_it_can_generate_keys
 
-    keys = KeysGenerator.new
+    keys = KeysGenerator.new("02715")
     expected = {A: "02", B: "27", C: "71", D: "15"}
-    assert_equal expected, keys.generate_keys("02715")
+    assert_equal expected, keys.keys
   end
 
 end
