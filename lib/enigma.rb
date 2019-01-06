fdcb79741fe50b4d4f3e9649fd528c3f88b10b81
Julia Marco <marcootj@gmail.com>
2019-01-06 16:06:54 -0700

    Refactors decrypt method into smaller chunks

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,6 +8,7 @@ class Enigma < ShiftGenerator
     @character_set = ("a".."z").to_a << " "
     @shifts = {}
     @encrypted = []
+    @decrypted = []
   end
 
   def todays_date
@@ -55,31 +56,26 @@ class Enigma < ShiftGenerator
     end
   end
 
-  # def decrypt(string, keys, offsets)
-  #   generate_shifts(keys, offsets)
-  #   index_hash = sort_char_shift_by_index(string)
-  #   decrypted = string.chars.map.with_index do |char, index|
-  #     if index_hash[:A].include?(index)
-  #       index = @character_set.find_index(char)
-  #       rotated = @character_set.rotate(-@shifts[:A])
-  #       rotated[index]
-  #     elsif index_hash[:B].include?(index)
-  #       index = @character_set.find_index(char)
-  #       rotated = @character_set.rotate(-@shifts[:B])
-  #       rotated[index]
-  #     elsif index_hash[:C].include?(index)
-  #       index = @character_set.find_index(char)
-  #       rotated = @character_set.rotate(-@shifts[:C])
-  #       rotated[index]
-  #     else index_hash[:D].include?(index)
-  #       index = @character_set.find_index(char)
-  #       rotated = @character_set.rotate(-@shifts[:D])
-  #       rotated[index]
-  #     end
-  #   end
-  #   {decryption: decrypted.join, key: keys, date: offsets}
-  # end
+  def decrypt(string, keys, offsets)
+    generate_shifts(keys, offsets)
+    index_hash = sort_char_shift_by_index(string)
+    string.chars.each_with_index do |char, index|
+      index_hash.each do |letter, idx|
+        if @character_set.include?(char)
+          if index_hash[letter].include?(index)
+            build_decrypted_message(letter, char)
+          end
+        end
+      end
+    end
+    {decryption: @decrypted.join, key: keys, date: offsets}
+  end
 
+  def build_decrypted_message(letter_shift, char)
+    index = @character_set.find_index(char)
+    rotated = @character_set.rotate(-@shifts[letter_shift])
+    @decrypted << rotated[index]
+  end
 
 
 end
