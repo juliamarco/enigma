e1e92630401feecc1753f5786553502bedf15e5a
Julia Marco <marcootj@gmail.com>
2019-01-06 14:20:09 -0700

    Changed class so they generate keys or offsets as soon as they are initialized

--- lib/keys_generator.rb
+++ lib/keys_generator.rb
@@ -1,14 +1,15 @@
 class KeysGenerator
   attr_reader :keys
 
-  def initialize
-    @keys = {A: 0, B: 0, C: 0, D: 0}
+  def initialize(number)
+    @keys = generate_keys(number)
   end
 
   def generate_keys(number)
+    keys = {A: 0, B: 0, C: 0, D: 0}
     number = number.to_s.split("")
-    @keys.each do |letter, num|
-      @keys[letter] = number.slice(0..1).join
+    keys.each do |letter, num|
+      keys[letter] = number.slice(0..1).join
       number.shift
     end
   end
--- lib/offsets_generator.rb
+++ lib/offsets_generator.rb
@@ -1,15 +1,16 @@
 class OffsetsGenerator
   attr_reader :offsets
 
-  def initialize
-    @offsets = {A: 0, B: 0, C: 0, D: 0}
+  def initialize(date)
+    @offsets = generate_offsets(date)
   end
 
   def generate_offsets(date)
+    offsets = {A: 0, B: 0, C: 0, D: 0}
     date = date.to_i
     number = (date * date).to_s[-4..-1].split("")
-    @offsets.each do |letter, num|
-      @offsets[letter] = number[0]
+    offsets.each do |letter, num|
+      offsets[letter] = number[0]
       number.shift
     end
   end
