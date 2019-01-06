5f8d3ec6f013e9e5db9e871e7a32b853e793ba83
Julia Marco <marcootj@gmail.com>
2019-01-06 13:54:14 -0700

    added test_it_exists for offsets_generator_test

--- test/offsets_generator_test.rb
+++ test/offsets_generator_test.rb
@@ -4,4 +4,10 @@ require 'minitest/pride'
 
 class OffsetsGeneratorTest < MiniTest::Test
 
+  def test_it_exists
+
+    offsets = OffsetsGenerator.new
+    assert_instance_of OffsetsGenerator, offsets
+  end
+
 end
