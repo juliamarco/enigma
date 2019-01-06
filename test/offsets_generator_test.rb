750b2429293fa709ceea70ccd6be8789cb22a7c6
Julia Marco <marcootj@gmail.com>
2019-01-06 13:57:01 -0700

    added test_it_starts_with_no_offsets

--- test/offsets_generator_test.rb
+++ test/offsets_generator_test.rb
@@ -10,4 +10,11 @@ class OffsetsGeneratorTest < MiniTest::Test
     assert_instance_of OffsetsGenerator, offsets
   end
 
+  def test_it_starts_with_no_offsets
+
+    offsets = OffsetsGenerator.new
+    expected = {A: 0, B: 0, C: 0, D: 0}
+    assert_equal expected, offsets.offsets
+  end
+
 end
