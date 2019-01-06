839238fe2637645bd376c0e0a667fd78f75a453e
Julia Marco <marcootj@gmail.com>
2019-01-06 13:47:15 -0700

    Added test_it_starts_with_no_keys

--- test/keys_generator_test.rb
+++ test/keys_generator_test.rb
@@ -10,4 +10,11 @@ class KeysGeneratorTest < MiniTest::Test
     assert_instance_of KeysGenerator, keys
   end
 
+  def test_it_starts_with_no_keys
+
+    keys = KeysGenerator.new
+    expected = {A: 0, B: 0, C: 0, D: 0}
+    assert_equal expected, keys.keys
+  end
+
 end
