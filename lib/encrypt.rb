09ca485f0349132678aeaea12c70558b48d1d2f4
Julia Marco <marcootj@gmail.com>
2019-01-07 10:20:45 -0700

    moved message.txt to the root directory

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,4 @@
-require './enigma.rb'
+require './lib/enigma'
 require 'date'
 
 handle = File.open(ARGV[0], "r")
@@ -6,11 +6,10 @@ message = handle.read.chop
 handle.close
 
 enigma = Enigma.new
-encrypted = enigma.encrypt(message)
+encrypted = enigma.encrypt(message,"82648", "240818")
 
 file_name = ARGV[1]
 writer = File.open(file_name, "w")
-
 writer.write(encrypted[:encryption])
 
 puts "Created '#{file_name}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
