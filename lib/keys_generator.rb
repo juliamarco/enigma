bc98706a2fc9fb6dfbde8b95d8ae0921a4025b04
Julia Marco <marcootj@gmail.com>
2019-01-08 16:51:15 -0700

    Added generate_keys_as a separate method from initialize and added random number method inside this class

--- lib/keys_generator.rb
+++ lib/keys_generator.rb
@@ -2,14 +2,25 @@ class KeysGenerator
   attr_reader :keys
 
   def initialize(number)
-    @keys = generate_keys(number)
+    @keys = {A: 0, B: 0, C: 0, D: 0}
   end
 
   def generate_keys(number)
-    keys = {A: 0, B: 0, C: 0, D: 0}
+    if number == 0
+      number = random_number
+    end
+    assign_num_to_letters(number)
+  end
+
+  def random_number
+    number = rand(99999)
+    "%05d" % number.to_s
+  end
+
+  def assign_num_to_letters(number)
     number = number.to_s.split("")
-    keys.each do |letter, num|
-      keys[letter] = number.slice(0..1).join
+    @keys.each do |letter, num|
+      @keys[letter] = number.slice(0..1).join
       number.shift
     end
   end
